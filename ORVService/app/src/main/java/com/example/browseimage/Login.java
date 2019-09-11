694288ba91800cbb905abfab3e12d3e306ecfeea
chandanprasad081 <48102837+chandanprasad081@users.noreply.github.com>
2019-09-11 01:12:04 -0800

    Business register

--- ORVService/app/src/main/java/com/example/browseimage/Login.java
+++ ORVService/app/src/main/java/com/example/browseimage/Login.java
@@ -3,19 +3,45 @@ package com.example.browseimage;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-EditText et1;
+    EditText et1;
+    TextView createaccount;
+    String value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        et1=findViewById(R.id.userid);
+        et1 = findViewById(R.id.userid);
+        createaccount = findViewById(R.id.createaccount);
 
-//        Intent intent=getIntent();
-//        String value=intent.getStringExtra("user");
+        Intent intent = getIntent();
+        value = intent.getStringExtra("name");
 //        et1.setText(value);
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (value.equals("user")) {
+                    Intent intent=new Intent(Login.this,Create_business.class);
+                    startActivity(intent);
+                }
+                else if (value.equals("admin")) {
+                    Intent intent=new Intent(Login.this,Create_business.class);
+                    startActivity(intent);
+                }
+                else if (value.equals("business")) {
+                    Intent intent=new Intent(Login.this,Business_registration.class);
+                    startActivity(intent);
+                }
+                else {
+
+                }
+            }
+        });
     }
 
 }
